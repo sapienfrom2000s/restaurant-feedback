5731a6369bc4c7d6ca5f59e682dfb6eae003f20f
brickwall <sk292207@gmail.com>
2023-12-16 00:34:37 +0530

    Add background to page

--- src/App.jsx
+++ src/App.jsx
@@ -31,35 +31,40 @@ function App() {
     <li><a href="">Reservations</a></li>
     <li><a href=''>Opening Hours</a></li>
     <li><a href="">Find Us</a></li>
+    <li><a href="">Order</a></li>
     </div>
     </ul>
       <div className="content">
-      <h2>Restaurant Feedback</h2>
-      <RatingInput
-        label="Food Quality"
-        value={ratings.foodQuality}
-        onChange={(e) => handleRatingChange('foodQuality', e)}
-      />
-      <RatingInput
-        label="Ambience"
-        value={ratings.ambience}
-        onChange={(e) => handleRatingChange('ambience', e)}
-      />
-      <RatingInput
-        label="Value for Money"
-        value={ratings.valueForMoney}
-        onChange={(e) => handleRatingChange('valueForMoney', e)}
-      />
-      <RatingInput
-        label="Hygiene & Safety"
-        value={ratings.hygieneAndSafety}
-        onChange={(e) => handleRatingChange('hygieneAndSafety', e)}
-      />
-      <Comment 
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
-        />
-    </div>
+        <div className="box">
+          <h2>Your feedback is important to us!</h2>
+          <RatingInput
+            label="Food Quality"
+            value={ratings.foodQuality}
+            onChange={(e) => handleRatingChange('foodQuality', e)}
+          />
+          <RatingInput
+            label="Ambience"
+            value={ratings.ambience}
+            onChange={(e) => handleRatingChange('ambience', e)}
+          />
+          <RatingInput
+            label="Value for Money"
+            value={ratings.valueForMoney}
+            onChange={(e) => handleRatingChange('valueForMoney', e)}
+          />
+          <RatingInput
+            label="Hygiene & Safety"
+            value={ratings.hygieneAndSafety}
+            onChange={(e) => handleRatingChange('hygieneAndSafety', e)}
+          />
+          <Comment 
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            />
+        </div>
+      </div>
+      <div className="background">
+      </div>
     </div>
   );
 
